75a97abcdee8657f550c07b0127e9e222323a2c5
Omar Alshammry <mskilldx@gmail.com>
2024-07-28 12:30:45 +0300

    done with home fragment and notifications

--- app/src/main/java/com/mbsstudentscheduler/homeFragment.java
+++ app/src/main/java/com/mbsstudentscheduler/homeFragment.java
@@ -1,12 +1,18 @@
 package com.mbsstudentscheduler;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +21,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import java.util.Calendar;
 
-//TODO: make the counter automatically update and implement a notification system
 
 public class homeFragment extends Fragment {
     private final int millisInWeek = 604740000;
@@ -35,8 +42,7 @@ public class homeFragment extends Fragment {
         TextView timer = view.findViewById(R.id.home_text_timer);
 
         dbloader();
-        setUpCounter(homeClass,homeRoom,timer);
-
+        setUpCounter(homeClass,homeRoom,timer,getContext());
         return view;
     }
 
@@ -103,7 +109,7 @@ public class homeFragment extends Fragment {
             }
         }
     }
-    private void setUpCounter(TextView homeClass, TextView homeRoom, TextView timer){
+    private void setUpCounter(TextView homeClass, TextView homeRoom, TextView timer, Context context){
         if (scheduleElement.SCArraylist.isEmpty()){
             homeClass.setText("No classes set");
             homeRoom.setText("No class set");
@@ -112,9 +118,9 @@ public class homeFragment extends Fragment {
         else {
             int i = 0;
             if (getTimeMills() > scheduleElement.SCArraylist.get(scheduleElement.SCArraylist.size() - 1).getStartTime()) { //if the last start time in the array is less than the current time
-                homeClass.setText(scheduleElement.SCArraylist.get(0).getClassID());
-                homeRoom.setText(scheduleElement.SCArraylist.get(0).getRoomID());
-                setFormatTime(timer, (scheduleElement.SCArraylist.get(0).getStartTime() + millisInWeek) - getTimeMills());
+                homeClass.setText(scheduleElement.SCArraylist.get(i).getClassID());
+                homeRoom.setText(scheduleElement.SCArraylist.get(i).getRoomID());
+                setFormatTime(timer, (scheduleElement.SCArraylist.get(i).getStartTime() + millisInWeek) - getTimeMills());
             } else {
                 while (getTimeMills() > scheduleElement.SCArraylist.get(i).getStartTime()) {
                     i++;
@@ -123,22 +129,56 @@ public class homeFragment extends Fragment {
                 homeRoom.setText(scheduleElement.SCArraylist.get(i).getRoomID());
                 setFormatTime(timer, scheduleElement.SCArraylist.get(i).getStartTime() - getTimeMills());
             }
-            if (getTimeMills()-scheduleElement.SCArraylist.get(i).getStartTime()==300000){
-            }
-            refresh(homeClass, homeRoom, timer);
+            notification(getTimeMills(),scheduleElement.SCArraylist.get(i),context);
+            // The alarm function goes here
+            refresh(homeClass, homeRoom, timer,context);
         }
     }
 
-    private void refresh(TextView homeClass, TextView homeRoom, TextView timer){
+    private void refresh(TextView homeClass, TextView homeRoom, TextView timer,Context context){
         final Handler handler = new Handler();
 
         final Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                setUpCounter(homeClass,homeRoom,timer);
+                setUpCounter(homeClass,homeRoom,timer,context);
             }
         };
         handler.postDelayed(runnable, 1000);
+    }
+
+    private void notification(int currentTime, scheduleElement element, Context context){
+        if (!element.isMuteState()){
+            if (currentTime==element.getStartTime()-300000){
+
+                String channelID = "STUDENT-SCHEDULER-APP";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context,channelID)
+                        .setSmallIcon(R.drawable.ic_start_time)
+                        .setContentTitle(element.getClassID())
+                        .setContentText("Class "+element.getClassID()+ " in room "+element.getRoomID()+" in 5 minutes")
+                        .setPriority(NotificationCompat.PRIORITY_MAX);
+
+                Intent intent = new Intent(context, homeFragment.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_MUTABLE);
+                builder.setContentIntent(pendingIntent);
+                NotificationManager notificationManager = (NotificationManager) requireActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+
+                NotificationChannel notificationChannel = notificationManager.getNotificationChannel(channelID);
+                if (notificationChannel == null){
+                    int importance = notificationManager.IMPORTANCE_HIGH;
+                    notificationChannel = new NotificationChannel(channelID,"some description", importance);
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+                notificationManager.notify(0,builder.build());
+                element.setMuteState(true);
+            }
+            else if (currentTime<element.getStartTime()-300000){
+                element.setMuteState(false);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/mbsstudentscheduler/settingsFragment.java
+++ app/src/main/java/com/mbsstudentscheduler/settingsFragment.java
@@ -13,7 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.Objects;
-
+//TODO: I need to make the settings tab actually work with the other tabs
 public class settingsFragment extends Fragment{
     
     @Nullable
